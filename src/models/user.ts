d6ba05d0ca267c455d54214d2551adf8638cea7c
Akash Khare <akashkhare706882@gmail.com>
2024-06-29 12:50:01 +0530

    first commit

---
+++ src/models/user.ts
@@ -0,0 +1,42 @@
+
+
+
+import { ObjectId } from "mongodb";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  
+    // _id: ObjectId,
+
+    auth0Id: {
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        
+    },
+    phone: {
+        type: Number,
+        
+    },
+    addressLine1: {
+        type: String,
+        
+    },
+    city: {
+        type: String,
+        
+    },
+    country: {
+        type: String,
+    }
+
+});
+
+const User =  mongoose.model("User", userSchema);
+export default User; 
