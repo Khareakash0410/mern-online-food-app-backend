9ee517aced0ce4c7cfdb1c3e9e1d89ef3dfe4a3c
Akash Khare <akashkhare706882@gmail.com>
2024-06-30 18:23:40 +0530

    TODAY COMMIT

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -16,7 +16,7 @@ export const validateMyUserRequest = [
 
     body("name").isString().notEmpty().withMessage("Name must be a string"),
 
-    body("phone").isNumeric().notEmpty().withMessage("Phone must be a number"),
+    body("phone").isInt({ min: 0 }).withMessage("Phone must be a number"),
 
     body("addressLine1").isString().notEmpty().withMessage("AddressLine1 must be a string"),
 
@@ -27,3 +27,26 @@ export const validateMyUserRequest = [
     handleValidationErrors,
 ];
 
+export const validateMyRestaurantRequest = [
+
+
+  body("restaurantName").notEmpty().withMessage("Restaurant name is required"),
+
+  body("city").notEmpty().withMessage("City is required"),
+
+  body("country").notEmpty().withMessage("Country is required"),
+
+  body("delieveryPrice").isFloat({ min: 0 }).withMessage("Delievery price must be a positive number"),
+
+  body("estimatedDelieveryTime").isInt({ min: 0 }).withMessage("Estimated delievery time must be a positive integer"),
+
+  body("cuisines").isArray().withMessage("Cuisines must be an array").not().isEmpty().withMessage("Cuisines array cannot be empty"),
+
+  body("menuItems").isArray().withMessage("Menu items must be an array"),
+
+  body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+
+  body("menuItems.*.price").isFloat({ min: 0 }).withMessage("Menu item price is required and must be positive number"),
+
+  handleValidationErrors,
+];
