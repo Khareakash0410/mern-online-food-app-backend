9536ec7a085a8b6febebe108e79d98a1ef285588
Akash Khare <akashkhare706882@gmail.com>
2024-07-03 17:24:18 +0530

    added search feature

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import express, {Request, Response} from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from "./routes/MyUserRoute"
+import myUserRoute from "./routes/MyUserRoute";
 import { v2 as cloudinary } from "cloudinary";
-import myRestaurantRoute from "./routes/MyRestaurantRoute"
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("connected to database!!!"));
 
@@ -29,6 +30,8 @@ app.get("/health", async (req: Request, res: Response) => {
 //   /api/my/user
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
+
 
 
 app.listen(7000, ()=> {
