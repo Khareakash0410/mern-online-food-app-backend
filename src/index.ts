b037fe620f0435aca3070ca8a575e1527becd9ec
Akash Khare <akashkhare706882@gmail.com>
2025-02-16 12:36:27 +0530

    update cors

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,10 @@ cloudinary.config({
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {origin: "http://localhost:5173",
+    credentials: true}
+));
 
 
 app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
